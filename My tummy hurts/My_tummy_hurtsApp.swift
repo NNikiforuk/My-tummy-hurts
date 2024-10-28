59529ef33cfbcc010a60a5cd447a945910e997be
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-29 00:11:35 +0100

    Initial Commit

---
+++ My tummy hurts/My_tummy_hurtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_tummy_hurtsApp.swift
+//  My tummy hurts
+//
+//  Created by Natalia Nikiforuk on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct My_tummy_hurtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

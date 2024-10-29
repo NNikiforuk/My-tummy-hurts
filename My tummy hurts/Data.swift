2400ba9e66c1d55a4266880150a497d32c1aa19a
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-29 14:52:56 +0100

    Types for data in .json file

--- My tummy hurts/Data.swift
+++ My tummy hurts/Data.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+
+
+struct Data: Codable, Hashable {
+    var user: String
+    var data: [DataEntries]
+    
+    struct DataEntries: Codable, Hashable {
+        var date: String
+        var entries: [Entry]
+        
+        struct Entry: Codable, Hashable {
+            var id: Int
+            var type: String
+            var time: String
+            var ingredients: [String]
+            var symptoms: [Symptom]
+            
+            struct Symptom: Codable, Hashable {
+                var time: String
+                var description: String
+            }
+        }
+    }
+}

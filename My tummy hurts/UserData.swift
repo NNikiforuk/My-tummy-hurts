6e43d55fb693339bb1eb13061eadff70543aa37c
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-29 15:03:29 +0100

    ModelData

--- My tummy hurts/UserData.swift
+++ My tummy hurts/UserData.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct UserData: Codable, Hashable {
+    var date: String
+    var entries: [Entry]
+    
+    struct Entry: Codable, Hashable {
+        var id: Int
+        var type: String
+        var time: String
+        var ingredients: [String]
+        var symptoms: [Symptom]
+        
+        struct Symptom: Codable, Hashable {
+            var time: String
+            var description: String
+        }
+    }
+}
+
+

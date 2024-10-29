5a18c481dacd49fc73f6b4ca8d4ad72f507ce9b2
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-29 23:19:08 +0100

    Calendar into a separate view

---
+++ My tummy hurts/Views/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  My tummy hurts
+//
+//  Created by Natalia Nikiforuk on 29/10/2024.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}

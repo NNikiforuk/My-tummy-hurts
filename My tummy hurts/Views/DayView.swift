f9eb93393ee9c929fcea163120c00078eba3662a
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-30 12:45:35 +0100

    New View for selecting a day

---
+++ My tummy hurts/Views/DayView.swift
@@ -0,0 +1,18 @@
+//
+//  DayView.swift
+//  My tummy hurts
+//
+//  Created by Natalia Nikiforuk on 29/10/2024.
+//
+
+import SwiftUI
+
+struct DayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DayView()
+}
---
+++ My tummy hurts/Views/SelectDayView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectDayView.swift
+//  My tummy hurts
+//
+//  Created by Natalia Nikiforuk on 30/10/2024.
+//
+
+import SwiftUI
+
+struct SelectDayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectDayView()
+}
